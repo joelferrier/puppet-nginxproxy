b810179883b34c09c1ae62f8c092234c9c22de3e
Joel Ferrier <jferrier45@gmail.com>
2015-03-23 14:04:56 -0700

    fix typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,15 +12,15 @@ class nginxproxy(
   $rewrite_port          = "443",
   )  {
   include wildcardssl
-  include iptables
+  #include iptables
 
-  iptables::rule_fragment { '10web':
-    source  => 'puppet:///modules/nginxproxy/web.rules',
-  }
+  #iptables::rule_fragment { '10web':
+  #  source  => 'puppet:///modules/nginxproxy/web.rules',
+  #}
 
-  iptables::rule_fragment { '20status':
-    source => 'puppet:///modules/nginxproxy/status.rules'
-  }
+  #iptables::rule_fragment { '20status':
+  #  source => 'puppet:///modules/nginxproxy/status.rules'
+  #}
 
   package { 'nginx':  ensure => installed, }
 
@@ -76,7 +76,7 @@ class nginxproxy(
 
   exec { 'semodule-nginx':
     user        => 'root',
-    commant     => '/sbin/semanage -i /root/nginxprox.pp',
+    command     => '/sbin/semanage -i /root/nginxprox.pp',
     refreshonly => true,
     require     => File['selinux-module'],
   }
